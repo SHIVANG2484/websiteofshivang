30e6fbd7d4be6bbe43c542c268763fc635659511
SHIVANG2484 <70949319+SHIVANG2484@users.noreply.github.com>
2021-04-06 10:38:17 +0530

    SHIVANG SHARMA

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings:  ["INDIAN"],
+        strings:  ["INDIAN","AJSHHD","HHHHHD"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
